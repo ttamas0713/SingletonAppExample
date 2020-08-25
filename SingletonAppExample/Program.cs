d3ba0d7d0b182cef0188e22b3b23a9dc2da2ae22
Tamás Tóth <ttamas0713@gmail.com>
2020-08-25 17:03:39 +0200

    Add project files.

---
+++ SingletonAppExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SingletonAppExample
+{
+    /// <summary>
+    /// This program is an example of singleton pattern
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.createSingleton();
+            Singleton singleton2 = Singleton.createSingleton();
+
+            Console.WriteLine(Singleton.instanceCounter);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonAppExample/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonAppExample
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        public static int instanceCounter { get; private set; } = 0;
+        private Singleton()
+        {
+            instanceCounter++;
+        }
+        public static Singleton createSingleton()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
